b4bdd0ee98fc9583ee93bb0f85560a0b4acbcedb
Artem <shentiariakova@mail.ru>
2026-03-01 18:23:19 +0400

    Добавил .gitignore и удалил venv и __pycache__

--- app/main.py
+++ app/main.py
@@ -17,10 +17,53 @@ path_to_json = os.path.join(parent_dir, 'blog.json')
 app = FastAPI()
 
 
-@app.get("/blogs/") # Обозначает, что эта функция будет обрабатывать GET запросы по пути /notes/
-def get_all_blogs(): # Когда клиент отправляет GET запрос на /notes/, эта функция будет вызвана
-    return json_to_dict_list(path_to_json) # Возвращаем все заметки
+@app.get("/blogs/")
+def get_all_blogs():
+    return json_to_dict_list(path_to_json)
+
 
 @app.get("/")
 def home_page():
     return {"message": "Добро пожаловать в API блогов!"}
+
+
+@app.get("/blogs/post/{category_id}")
+def get_posts(category_id: int, author: Optional[int] = None, category: Optional[int] = None, location: Optional[int] = None):
+    blogs = json_to_dict_list(path_to_json)
+    filtered_posts = []
+    for blog in blogs:
+        if blog.get("model") == "blog.post" and blog.get("fields", {}).get("category") == category_id:
+            filtered_posts.append(blog)
+    if author is not None:
+        filtered_posts = [post for post in filtered_posts if post["fields"]["author"] == author]
+        
+    if location is not None:
+        filtered_posts = [post for post in filtered_posts if post['fields']['location'] == location]
+        
+    return filtered_posts
+
+
+@app.get("/blogs/category/{is_published}")
+def get_categories(is_published: bool, title: Optional[str] = None):
+    blogs = json_to_dict_list(path_to_json)
+    filtered_categories = []
+    for blog in blogs:
+        if blog.get("model") == "blog.category" and blog["fields"]["is_published"] == is_published:
+            filtered_categories.append(blog)
+        
+    if title is not None:
+        filtered_categories = [category for category in filtered_categories if title.lower() in category['fields']['title'].lower()]
+        
+    return filtered_categories
+
+
+@app.get("/blogs/location/{is_published}")
+def get_locations(is_published: bool, name: Optional[str] = None):
+    blogs = json_to_dict_list(path_to_json)
+    filtered_locations = []
+    for blog in blogs:
+        if blog.get("model") == "blog.location" and blog["fields"]["is_published"] == is_published:
+            filtered_locations.append(blog)
+    if name is not None:
+        filtered_locations = [location for location in filtered_locations if name.lower() in location['fields']['name'].lower()]
+    return filtered_locations
